8d0ad8d3cf0586dfccb6e31f08ea34ea8eccbe8d
Piotr Zalewa <piotr@zalewa.info>
2012-05-03 19:53:27 +0200

    * location as a button * do not create new marker every time location is sarched for

--- js/app.js
+++ js/app.js
@@ -3,14 +3,19 @@ window.onload=function(){
         settings.zoom = map.getZoom();
     });
 
-    var my = document.getElementById('mylocation');
-    my.addEventListener('click', function(e) {
+    document.getElementById('btnLocation').addEventListener('click', function(e) {
         if (navigator.geolocation) {  
             navigator.geolocation.getCurrentPosition(function(position) {  
                 map.setView(new L.LatLng(position.coords.latitude, position.coords.longitude), settings.zoom);
-                // create a marker in the given location and add it to the map
-                var marker = new L.Marker(new L.LatLng(position.coords.latitude, position.coords.longitude));
-                map.addLayer(marker);
+                if (!map.currentLocationMarker) {
+                    // create a marker in the given location and add it to the map
+                    map.currentLocationMarker = new L.Marker(
+                        new L.LatLng(position.coords.latitude, position.coords.longitude));
+                    map.addLayer(map.currentLocationMarker);
+                } else {
+                    map.currentLocationMarker.setLatLng(
+                        new L.LatLng(position.coords.latitude, position.coords.longitude))
+                }
             });
         } else {  
           alert("I'm sorry, but geolocation services are not supported by your browser.");  
