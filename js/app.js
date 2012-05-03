d9f7cd356adab6a486c5ad15e0de54142353b6fb
Piotr Zalewa <piotr@zalewa.info>
2012-05-03 20:01:12 +0200

    app.js

---
+++ js/app.js
@@ -0,0 +1,15 @@
+window.onload=function(){
+    var my = document.getElementById('mylocation');
+    my.addEventListener('click', function(e) {
+        if (navigator.geolocation) {  
+            navigator.geolocation.getCurrentPosition(function(position) {  
+                map.setView(new L.LatLng(position.coords.latitude, position.coords.longitude), settings.zoom);
+                // create a marker in the given location and add it to the map
+                var marker = new L.Marker(new L.LatLng(position.coords.latitude, position.coords.longitude));
+                map.addLayer(marker);
+            });
+        } else {  
+          alert("I'm sorry, but geolocation services are not supported by your browser.");  
+        } 
+    });
+};
