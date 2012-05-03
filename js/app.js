d4bb8e8f9df50273635fece9073ff40c2582e041
Arron Schaar <aschaar@gmail.com>
2012-05-03 11:35:17 -0700

    added search

--- js/app.js
+++ js/app.js
@@ -12,4 +12,41 @@ window.onload=function(){
           alert("I'm sorry, but geolocation services are not supported by your browser.");  
         } 
     });
+
+    var btn = document.getElementById('btnSearch');
+    btn.addEventListener('click', function(e){
+
+       var scriptTag = document.getElementById('geoSearchjsonp');
+       if (scriptTag){
+            document.getElementsByTagName('HEAD')[0].removeChild(scriptTag);
+        }
+        scriptTag = document.createElement('SCRIPT');
+        scriptTag.id = 'geoSearchjsonp';
+        scriptTag.src = "http://nominatim.openstreetmap.org/search?q="+
+            document.getElementById('txtSearch').value+
+            "&format=json&json_callback=geoSearchCallback";
+        document.getElementsByTagName('HEAD')[0].appendChild(scriptTag);
+
+    });
+    
+};
+
+function geoSearchCallback(result){
+    var location = null;
+    for (var i=0; i<result.length;i++){
+        if (result[i].type=="city" && result[i].osm_type=="node"){
+            location = result[i];
+            break;
+        }
+    }
+    if (!location){
+        location = result[0];
+    }
+    if (!location){
+        alert('unable to find location');
+    } else {
+        console.log(result);
+        map.setView(new L.LatLng(location.lat, location.lon), settings.zoom);
+    }
+    
 };
